39561afa9ccda0386af9dda855a85e5fc89c01d9
Shamayko <shamayko@gmail.com>
2021-06-02 11:07:24 +0300

    Дописал комментарии, чтобы можно было проверить правильно ли я понял код и добавил метод приватной отправки сообщений.

---
+++ src/main/java/HomeWork7v2/AuthService.java
@@ -0,0 +1,12 @@
+package HomeWork7v2;
+
+public interface AuthService {
+
+    //Создаем интерфейс, который контрактует нам методы старта, стопа и получения логина.
+    void start();
+
+    void stop();
+
+    String getNickByLoginAndPass(String login, String pass);
+}
+
---
+++ src/main/java/HomeWork7v2/ServerApp.java
@@ -0,0 +1,7 @@
+package HomeWork7v2;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
